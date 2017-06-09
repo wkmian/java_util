3cd1f1c89421725b68d64d1c7bde7c5fb0399d00
wkmian <wenkang0601@163.com>
2017-06-09 10:40:13 +0800

    fix

--- src/com/wkutil/wkstring/WKStringUtil.java
+++ src/com/wkutil/wkstring/WKStringUtil.java
@@ -16,42 +16,42 @@ public class WKStringUtil {
         return null==value;
 	}
 	
-	/**
-	 * 是否是空值 <code>null</code> or <code>""</code> 都视为空值
-	 * @param value 待检测的字符串
-	 * @return true空字符串
-	 */
+    /**
+     * 是否是空值 <code>null</code> or <code>""</code> 都视为空值
+     * @param value 待检测的字符串
+     * @return true空字符串
+     */
 	public static boolean isEmpty(String value){
 	    return isNull(value)||value.length()==0;
 	}
 	
-	/**
-	 * 如果字符串是<code>null</code>则返回<code>""</code>
-	 * 否则返回本身
-	 * @param str 待检测字符串
-	 * @return
-	 */
+    /**
+     * 如果字符串是<code>null</code>则返回<code>""</code>
+     * 否则返回本身
+     * @param str 待检测字符串
+     * @return
+     */
     public static String ifNull(String str){
         return ifNull(str, EMPTY_STRING);
     }
 	
-	/**
-	 * 如果字符串是<code>null</code>则返回<code>toStr</code>
-	 * 否则返回本身
-	 * @param str 待检测字符串
-	 * @param toStr 如果  null==str 替换 str
-	 * @return
-	 */
+    /**
+     * 如果字符串是<code>null</code>则返回<code>toStr</code>
+     * 否则返回本身
+     * @param str 待检测字符串
+     * @param toStr 如果  null==str 替换 str
+     * @return
+     */
     public static String ifNull(String str,String toStr){
         return null==str?toStr:str;
     }
 	
 
-	/**
-	 * 将String数组拼接成一个字符串
-	 * @param args 要拼接的字符串数组
-	 * @return
-	 */
+    /**
+     * 将String数组拼接成一个字符串
+     * @param args 要拼接的字符串数组
+     * @return
+     */
 	public static String join(String... args){
         String str = null;
         if(null!=args&&args.length>0){
